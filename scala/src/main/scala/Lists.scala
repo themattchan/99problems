be8dc2eb62425a751069f208cc83fc779b215a80
Matthew Chan <matt@themattchan.com>
2015-11-18 21:05:02 -0800

    more lists

--- scala/src/main/scala/Lists.scala
+++ scala/src/main/scala/Lists.scala
@@ -2,7 +2,10 @@ package org.hackwithlambda.problems
 
 /*
  * 99 Problems, 1-28.
- * Lists
+ * Lists.
+ *
+ * The exercise is to use as few builtin functions as possible,
+ * since we are re-implementing most of the standard list functions.
  */
 
 object Lists {
@@ -33,14 +36,44 @@ object Lists {
       case Nil => acc
       case h::t => go (h::acc, t)
     }
-    go(List(), xs)
+    go (List(), xs)
   }
 
   def isPalindrome[A](xs: List[A]): Boolean =
     xs == reverse(xs)
 
-  def flatten[A](xs: List[List[A]]): List[A] = xs flatMap identity
+  def flatten[A](xss: List[List[A]]): List[A] = {
+    def addAll[A](toAdd: List[A], acc: List[A]): List[A] = toAdd match {
+      case Nil => acc
+      case h::t => addAll(t, h::acc)
+    }
 
-  def compress
+    def go[A](acc: List[A], xss: List[List[A]]): List[A] = xss match {
+      case Nil => reverse(acc)
+      case xs::xss => go(addAll(xs,acc), xss)
+    }
 
+    go (List(), xss)
+  }
+
+  def compress[A](xs: List[A]): List[A] = {
+    def go[A](acc: List[A], xs: List[A]): List[A] = xs match {
+      case Nil => reverse(acc)
+      case h::t => if (h == acc.head) { go (acc, t) } else { go (h::acc,t) }
+    }
+    go (List(), xs)
+  }
+
+  def pack[A](xs: List[A]): List[List[A]] = {
+    def go[A](acc: List[A], xs: List[A]): List[List[A]] = xs match {
+      case Nil => reverse(acc)
+      case h::t =>
+        if (h == acc.head.head) {
+          go ((h::acc.head)::acc.tail, t)
+        } else {
+          go (List(h)::acc,t)
+        }
+    }
+    go (List(),xs)
+  }
 }
